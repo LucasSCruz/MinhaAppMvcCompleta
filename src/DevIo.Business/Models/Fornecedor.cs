51d999022157919d273ee4452d3d5e6757327619
Lucas <lucasscruz2001@gmail.com>
2022-04-25 10:11:19 -0300

    Criando projeto e adicionando a camada modelo

---
+++ src/DevIo.Business/Models/Fornecedor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DevIo.Business.Models
+{
+    public class Fornecedor : Entity
+    {
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public TipoFornecedor TipoForncedor { get; set; }
+        public Endereco Endereco { get; set; }
+        public bool Ativo { get; set; }
+
+        /* EF Relations */
+
+        public IEnumerable<Produto> Produtos { get; set; }
+    }
+}
